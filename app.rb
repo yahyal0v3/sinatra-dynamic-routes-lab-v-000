a0e63e38a1d9ba9bd580b7556fd9f54243488718
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-12-13 02:02:12 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,4 +11,9 @@ class App < Sinatra::Base
     @number = params[:number].to_i
     "#{@number * @number}"
   end
+
+  get 'say/:number/:phrase' do
+    @number = params[:number]
+    @phrase = params[:phrase]
+  end
 end
