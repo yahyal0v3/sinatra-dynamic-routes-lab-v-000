a9e35eda6db6e520db12baff4ef501a3137dbfde
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-12-13 01:49:09 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,5 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-
+  get '/reverse/:name' do 
+    @name = params[:name]
+    "#{@name.reverse}"
+  end
 end
